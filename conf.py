ea94942484e43a3169a760fb21ef939fa8c69f37
alex <consult@alexlittle.net>
2021-10-29 14:44:30 +0300

    update docs conf

--- conf.py
+++ conf.py
@@ -35,7 +35,10 @@ sys.path.append(os.path.abspath('../oppia/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage',]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
